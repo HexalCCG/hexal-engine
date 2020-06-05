9234a7f8c3ffe6da9ccfa1964316646416ed813e
Zoey Child <imperialoctopus@gmail.com>
2020-06-05 13:30:28 +0100

    Scaffold a console app

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:hexal_engine/hexal_engine.dart' as hexal_engine;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hexal_engine.calculate()}!');
+}
---
+++ test/hexal_engine_test.dart
@@ -0,0 +1,8 @@
+import 'package:hexal_engine/hexal_engine.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
