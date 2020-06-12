fc4bb9987d0ee09380fc14f201a67cca8131a61f
Zoey Child <imperialoctopus@gmail.com>
2020-06-12 19:30:59 +0100

    Add event test folder

---
+++ test/event_test/draw_card_event_test.dart
@@ -0,0 +1,35 @@
+import 'package:test/test.dart';
+
+import 'package:hexal_engine/game_state/game_info.dart';
+import 'package:hexal_engine/game_state/game_over_state.dart';
+import 'package:hexal_engine/game_state/game_state.dart';
+import 'package:hexal_engine/game_state/turn_phase.dart';
+import 'package:hexal_engine/actions/pass_action.dart';
+import 'package:hexal_engine/engine.dart';
+import 'package:hexal_engine/objects/player_object.dart';
+import 'package:hexal_engine/state_change/priority_state_change.dart';
+
+void main() {
+  group('Draw card event', () {
+    const p1 = PlayerObject(name: 'Alice');
+    const p2 = PlayerObject(name: 'Bob');
+    test('passes priority when used by the active player. ', () {
+      final state = const GameState(
+        gameInfo: GameInfo(
+          player1: p1,
+          player2: p2,
+        ),
+        gameOverState: GameOverState.playing,
+        cards: [],
+        stack: [],
+        activePlayer: p1,
+        priorityPlayer: p1,
+        turnPhase: TurnPhase.start,
+      );
+      //const action = PassAction();
+      //final change = Engine.processAction(state, action);
+
+      //expect(change, unorderedEquals(const [PriorityStateChange(player: p2)]));
+    });
+  });
+}
