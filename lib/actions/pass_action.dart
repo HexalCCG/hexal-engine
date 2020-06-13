d80e54a7a2cdef4a4c44be3993947f39aa490692
Zoey Child <imperialoctopus@gmail.com>
2020-06-13 20:01:08 +0100

    Refactor pass effects into combination files

--- lib/actions/pass_action.dart
+++ lib/actions/pass_action.dart
@@ -1,10 +1,5 @@
-import 'package:hexal_engine/event/draw_card_event.dart';
-import 'package:hexal_engine/state_change/add_stack_event_state_change.dart';
-
 import '../game_state/game_state.dart';
-import '../game_state/turn_phase.dart';
-import '../state_change/active_player_state_change.dart';
-import '../state_change/phase_state_change.dart';
+import '../state_change/combination/next_phase_state_changes.dart';
 import '../state_change/priority_state_change.dart';
 import '../state_change/state_change.dart';
 import 'action.dart';
@@ -22,28 +17,7 @@ class PassAction extends Action {
       // Non-active player has passed so resolve top stack event
       return state.resolveTopStackEvent();
     }
-    if (state.turnPhase != TurnPhase.end) {
-      if (state.turnPhase == TurnPhase.start) {
-        // Entering draw phase so add draw a card
-        return [
-          AddStackEventStateChange(
-              event: DrawCardEvent(player: state.activePlayer)),
-          PhaseStateChange(phase: TurnPhase.values[state.turnPhase.index + 1]),
-          PriorityStateChange(player: state.activePlayer)
-        ];
-      }
-      // Move on to the next phase
-      return [
-        PhaseStateChange(phase: TurnPhase.values[state.turnPhase.index + 1]),
-        PriorityStateChange(player: state.activePlayer)
-      ];
-    }
-    // Move on to the next turn
-    return [
-      ActivePlayerStateChange(player: state.notActivePlayer),
-      PhaseStateChange(phase: TurnPhase.start),
-      PriorityStateChange(player: state.notActivePlayer)
-    ];
+    return NextPhaseStateChanges.generate(state);
   }
 
   @override
