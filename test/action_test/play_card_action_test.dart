15812637c9c5f112b1ee1a8b208cd3ff319a7874
Zoey Child <imperialoctopus@gmail.com>
2020-06-24 17:04:15 +0100

    Add play card action test

---
+++ test/action_test/play_card_action_test.dart
@@ -0,0 +1,105 @@
+import 'package:hexal_engine/actions/play_card_action.dart';
+import 'package:hexal_engine/event/play_card_event.dart';
+import 'package:hexal_engine/exceptions/action_exception.dart';
+import 'package:hexal_engine/game_state/location.dart';
+import 'package:hexal_engine/objects/card_object.dart';
+import 'package:hexal_engine/state_change/add_stack_event_state_change.dart';
+import 'package:test/test.dart';
+
+import 'package:hexal_engine/game_state/player.dart';
+import 'package:hexal_engine/game_state/game_info.dart';
+import 'package:hexal_engine/game_state/game_over_state.dart';
+import 'package:hexal_engine/game_state/game_state.dart';
+import 'package:hexal_engine/game_state/turn_phase.dart';
+import 'package:hexal_engine/actions/pass_action.dart';
+import 'package:hexal_engine/objects/player_object.dart';
+import 'package:hexal_engine/state_change/phase_state_change.dart';
+import 'package:hexal_engine/state_change/priority_state_change.dart';
+
+void main() {
+  group('Play card action', () {
+    const p1 = PlayerObject(name: 'Alice');
+    const p2 = PlayerObject(name: 'Bob');
+
+    test('adds play card event to the stack. ', () {
+      const card = CardObject(
+        controller: Player.one,
+        owner: Player.one,
+        location: Location.hand,
+        enteredFieldThisTurn: false,
+      );
+      final state = const GameState(
+        gameInfo: GameInfo(
+          player1: p1,
+          player2: p2,
+        ),
+        gameOverState: GameOverState.playing,
+        cards: [card],
+        stack: [],
+        activePlayer: Player.one,
+        priorityPlayer: Player.one,
+        turnPhase: TurnPhase.main1,
+      );
+      const action = PlayCardAction(card: card);
+      final change = state.applyAction(action);
+
+      expect(
+          change,
+          containsAll(const [
+            AddStackEventStateChange(event: PlayCardEvent(card: card)),
+          ]));
+    });
+    test('fails if targetting card not in hand', () {
+      const card = CardObject(
+        controller: Player.one,
+        owner: Player.one,
+        location: Location.deck,
+        enteredFieldThisTurn: false,
+      );
+      final state = const GameState(
+        gameInfo: GameInfo(
+          player1: p1,
+          player2: p2,
+        ),
+        gameOverState: GameOverState.playing,
+        cards: [card],
+        stack: [],
+        activePlayer: Player.one,
+        priorityPlayer: Player.one,
+        turnPhase: TurnPhase.main1,
+      );
+      const action = PlayCardAction(card: card);
+
+      expect(
+        state.applyAction(action),
+        throwsA(isA<ActionException>()),
+      );
+    });
+    test('fails if used by non-active player', () {
+      const card = CardObject(
+        controller: Player.two,
+        owner: Player.two,
+        location: Location.hand,
+        enteredFieldThisTurn: false,
+      );
+      final state = const GameState(
+        gameInfo: GameInfo(
+          player1: p1,
+          player2: p2,
+        ),
+        gameOverState: GameOverState.playing,
+        cards: [card],
+        stack: [],
+        activePlayer: Player.one,
+        priorityPlayer: Player.two,
+        turnPhase: TurnPhase.main1,
+      );
+      const action = PlayCardAction(card: card);
+
+      expect(
+        state.applyAction(action),
+        throwsA(isA<ActionException>()),
+      );
+    });
+  });
+}
