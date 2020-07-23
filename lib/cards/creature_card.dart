539e33ec984ec8922335369f907418de4eaf8961
Zoey Child <imperialoctopus@gmail.com>
2020-07-23 23:26:30 +0100

    Make creature card abstract

--- lib/cards/creature_card.dart
+++ lib/cards/creature_card.dart
@@ -1,44 +1,15 @@
-import 'package:hexal_engine/game_state/location.dart';
-import 'package:hexal_engine/game_state/player.dart';
 import 'package:hexal_engine/objects/card_object.dart';
 
-class CreatureCard extends CardObject {
-  int get baseHealth => 0;
-  int get baseAttack => 0;
+abstract class CreatureCard extends CardObject {
+  int get baseHealth;
+  int get baseAttack;
 
   int get health => baseHealth - damage;
   int get attack => baseAttack;
 
-  final int damage;
+  int get damage;
 
-  const CreatureCard(
-      {Player owner,
-      Player controller,
-      Location location,
-      bool enteredFieldThisTurn,
-      this.damage})
-      : super(
-            owner: owner,
-            controller: controller,
-            location: location,
-            enteredFieldThisTurn: enteredFieldThisTurn);
-
-  @override
-  CreatureCard copyWith({
-    Player owner,
-    Player controller,
-    Location location,
-    bool enteredFieldThisTurn,
-    int damage,
-  }) {
-    return CreatureCard(
-      owner: owner ?? this.owner,
-      controller: controller ?? this.controller,
-      location: location ?? this.location,
-      enteredFieldThisTurn: enteredFieldThisTurn ?? this.enteredFieldThisTurn,
-      damage: damage ?? this.damage,
-    );
-  }
+  const CreatureCard();
 
   @override
   List<Object> get props =>
