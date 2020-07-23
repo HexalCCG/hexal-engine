d88b6c31c8730e71386014c858549f8ae3ccde70
Zoey Child <imperialoctopus@gmail.com>
2020-07-23 23:27:05 +0100

    Add first creature card class

--- lib/cards/sample/first_creature_card.dart
+++ lib/cards/sample/first_creature_card.dart
@@ -1,24 +1,44 @@
+import 'package:meta/meta.dart';
+
 import 'package:hexal_engine/cards/creature_card.dart';
 import 'package:hexal_engine/game_state/location.dart';
 import 'package:hexal_engine/game_state/player.dart';
 
 class FirstCreatureCard extends CreatureCard {
-  const FirstCreatureCard(
-      {Player owner,
-      Player controller,
-      Location location,
-      bool enteredFieldThisTurn,
-      int damage})
-      : super(
-            owner: owner,
-            controller: controller,
-            location: location,
-            enteredFieldThisTurn: enteredFieldThisTurn,
-            damage: damage);
+  @override
+  final Player owner;
+  @override
+  final Player controller;
+  @override
+  final Location location;
+  @override
+  final bool enteredFieldThisTurn;
+
+  @override
+  final int damage;
 
   @override
   int get baseAttack => 1;
 
   @override
   int get baseHealth => 1;
+
+  const FirstCreatureCard(
+      {@required this.owner,
+      @required this.controller,
+      @required this.location,
+      @required this.enteredFieldThisTurn,
+      @required this.damage});
+
+  @override
+  FirstCreatureCard copyWith(Map<String, dynamic> data) {
+    return FirstCreatureCard(
+      owner: data['owner'] ?? owner,
+      controller: data['controller'] ?? controller,
+      location: data['location'] ?? location,
+      enteredFieldThisTurn:
+          data['enteredFieldThisTurn'] ?? enteredFieldThisTurn,
+      damage: data['damage'] ?? damage,
+    );
+  }
 }
