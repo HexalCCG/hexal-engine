523a649c461df244758a973d616519deae04d6f3
Zoey Child <imperialoctopus@gmail.com>
2020-07-29 21:49:52 +0100

    Change OnCardEnterFieldEvent to new increment system

--- lib/event/on_card_enter_field_event.dart
+++ lib/event/on_card_enter_field_event.dart
@@ -1,54 +1,56 @@
+import 'package:hexal_engine/state_change/modify_event_state_change.dart';
 import 'package:meta/meta.dart';
 
 import '../cards/i_on_enter_field.dart';
 import '../game_state/game_state.dart';
 import '../objects/card_object.dart';
 import '../state_change/add_event_state_change.dart';
-import '../state_change/increment_event_state_change.dart';
 import '../state_change/resolve_event_state_change.dart';
 import '../state_change/state_change.dart';
 import 'event.dart';
-import 'i_incrementing.dart';
 
 /// Effect caused by a card entering the battlefield.
-class OnCardEnterFieldEvent extends Event implements IIncrementing {
+class OnCardEnterFieldEvent extends Event {
   final CardObject card;
-  @override
-  final int counter;
+  final int effectIndex;
 
   const OnCardEnterFieldEvent(
-      {@required this.card, this.counter = 0, bool resolved = false})
+      {@required this.card, this.effectIndex = 0, bool resolved = false})
       : super(resolved: resolved);
 
   @override
   List<StateChange> apply(GameState state) {
-    if (card is IOnEnterField) {
-      // If no valid effects are left
-      if (counter == (card as IOnEnterField).onEnterFieldEffects.length) {
-        return [ResolveEventStateChange(event: this)];
+    if (!(card is IOnEnterField)) {
+      // If the card has no enter field effects, resolve this.
+      return [ResolveEventStateChange(event: this)];
+    } else {
+      // If only one effect is left, do it and resolve
+      if (effectIndex ==
+          (card as IOnEnterField).onEnterFieldEffects.length - 1) {
+        return [
+          AddEventStateChange(
+              event: (card as IOnEnterField).onEnterFieldEffects[effectIndex]),
+          ResolveEventStateChange(event: this),
+        ];
       }
-      // Return the next effect and increment
+      // Otherwise do the effect and increment
       else {
         return [
           AddEventStateChange(
-              event: (card as IOnEnterField).onEnterFieldEffects[counter]),
-          IncrementEventStateChange(event: this),
+              event: (card as IOnEnterField).onEnterFieldEffects[effectIndex]),
+          ModifyEventStateChange(event: this, newEvent: _copyIncremented),
         ];
       }
-    } else {
-      // If the card has no enter field effects, resolve this.
-      return [ResolveEventStateChange(event: this)];
     }
   }
 
-  @override
-  OnCardEnterFieldEvent get copyResolved =>
-      OnCardEnterFieldEvent(card: card, counter: counter, resolved: true);
+  OnCardEnterFieldEvent get _copyIncremented => OnCardEnterFieldEvent(
+      card: card, effectIndex: effectIndex + 1, resolved: resolved);
 
   @override
-  OnCardEnterFieldEvent get increment => OnCardEnterFieldEvent(
-      card: card, counter: counter + 1, resolved: resolved);
+  OnCardEnterFieldEvent get copyResolved => OnCardEnterFieldEvent(
+      card: card, effectIndex: effectIndex, resolved: true);
 
   @override
-  List<Object> get props => [card, counter, resolved];
+  List<Object> get props => [card, effectIndex, resolved];
 }
