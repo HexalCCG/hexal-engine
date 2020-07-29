d445de4145b76feec1434fb92da4cf9c3c64eed0
Zoey Child <imperialoctopus@gmail.com>
2020-07-29 22:26:09 +0100

    Implement request target event apply function

--- lib/event/request_target_event.dart
+++ lib/event/request_target_event.dart
@@ -1,4 +1,8 @@
+import 'package:hexal_engine/effect/i_targetted.dart';
 import 'package:hexal_engine/effect/target/target.dart';
+import 'package:hexal_engine/exceptions/event_exceptipn.dart';
+import 'package:hexal_engine/state_change/modify_event_state_change.dart';
+import 'package:hexal_engine/state_change/resolve_event_state_change.dart';
 import 'package:meta/meta.dart';
 
 import '../effect/effect.dart';
@@ -9,13 +13,29 @@ import 'event.dart';
 class RequestTargetEvent extends Event {
   final Effect effect;
   final Target target;
+  final TargetResult targetResult;
 
-  const RequestTargetEvent(
-      {@required this.effect, @required this.target, bool resolved = false})
-      : super(resolved: resolved);
+  const RequestTargetEvent({
+    @required this.effect,
+    @required this.target,
+    this.targetResult,
+    bool resolved = false,
+  })  : assert(effect is ITargetted),
+        super(resolved: resolved);
 
   @override
-  List<StateChange> apply(GameState state) {}
+  List<StateChange> apply(GameState state) {
+    if (targetResult == null) {
+      throw const EventException(
+          'RequestTargetEvent error: TargetResult should not be null');
+    }
+    return [
+      ModifyEventStateChange(
+          event: effect,
+          newEvent: (effect as ITargetted).copyWithTarget(targetResult)),
+      ResolveEventStateChange(event: this),
+    ];
+  }
 
   @override
   RequestTargetEvent get copyResolved =>
