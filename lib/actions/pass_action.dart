af691a4b63de21d4a517563fe6de6ead2e9d0c33
Zoey Child <imperialoctopus@gmail.com>
2020-07-31 03:04:44 +0100

    Handle request target events in pass action

--- lib/actions/pass_action.dart
+++ lib/actions/pass_action.dart
@@ -1,3 +1,7 @@
+import 'package:hexal_engine/event/request_target_event.dart';
+import 'package:hexal_engine/exceptions/action_exception.dart';
+import 'package:hexal_engine/state_change/fill_request_state_change.dart';
+
 import '../game_state/game_state.dart';
 import '../state_change/combination/next_phase_state_changes.dart';
 import '../state_change/priority_state_change.dart';
@@ -9,6 +13,25 @@ class PassAction extends Action {
 
   @override
   List<StateChange> apply(GameState state) {
+    // Check if the top event is a request.
+    if (state.stack.isNotEmpty) {
+      var topStackEvent = state.stack.last;
+      if (topStackEvent is RequestTargetEvent &&
+          topStackEvent.target.controller == state.priorityPlayer) {
+        // Top event is a request that we control.
+        if (topStackEvent.target.targetValid(null)) {
+          return [
+            FillRequestStateChange(
+                request: topStackEvent,
+                targetResult: topStackEvent.target.createResult(null))
+          ];
+        } else {
+          throw const ActionException(
+              'PassAction Exception: Cannot pass non-optional target request.');
+        }
+      }
+    }
+
     if (state.priorityPlayer == state.activePlayer) {
       // Active player has passed so check for non-active response
       return [PriorityStateChange(player: state.notPriorityPlayer)];
