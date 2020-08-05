91020dbb6a8d380966e0b2ece0452e6e511453c4
Zoey Child <imperialoctopus@gmail.com>
2020-08-05 12:54:17 +0100

    Refactor validation in provide target action

--- lib/actions/provide_target_action.dart
+++ lib/actions/provide_target_action.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
-import 'package:hexal_engine/event/request_target_event.dart';
+
+import '../event/request_target_event.dart';
 import '../exceptions/action_exception.dart';
 import '../game_state/game_state.dart';
 import '../state_change/state_change.dart';
@@ -11,30 +12,33 @@ class ProvideTargetAction extends Action {
   const ProvideTargetAction({@required this.target});
 
   @override
-  List<StateChange> apply(GameState state) {
-    // Check if the top event is a request.
-    if (state.stack.isNotEmpty) {
-      var topStackEvent = state.stack.last;
-      if (topStackEvent is RequestTargetEvent &&
-          topStackEvent.targetResult == null &&
-          topStackEvent.target.controller == state.priorityPlayer) {
-        // Top event is a request that we control.
-        if (topStackEvent.target.targetValid(target)) {
-          return [
-            ...topStackEvent.createFillStateChange(target),
-          ];
-        } else {
-          throw const ActionException(
-              'ProvideTargetAction Exception: Invalid target provided.');
-        }
-      } else {
-        throw const ActionException(
-            ('ProvideTargetAction Exception: Request could not be filled'));
-      }
-    } else {
-      throw const ActionException(
-          ('ProvideTargetAction Exception: Top event is not a request'));
+  bool valid(GameState state) {
+    if (state.stack.isEmpty || !(state.stack.last is RequestTargetEvent)) {
+      // Top event is not a request.
+      return false;
     }
+    if ((state.stack.last as RequestTargetEvent).targetResult != null ||
+        (state.stack.last as RequestTargetEvent).target.controller !=
+            state.priorityPlayer) {
+      // Request is not yours or has been filled already.
+      return false;
+    }
+    if (!(state.stack.last as RequestTargetEvent).target.targetValid(target)) {
+      // Target is not valid.
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  List<StateChange> apply(GameState state) {
+    if (!valid(state)) {
+      throw const ActionException(
+          'ProvideTargetAction Exception: invalid argument');
+    }
+    return [
+      ...(state.stack.last as RequestTargetEvent).createFillStateChange(target),
+    ];
   }
 
   @override
