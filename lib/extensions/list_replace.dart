77045630f25606524c899732206ae7636894adfc
Zoey Child <imperialoctopus@gmail.com>
2020-08-06 12:58:40 +0100

    Document list_replace

--- lib/extensions/list_replace.dart
+++ lib/extensions/list_replace.dart
@@ -1,4 +1,7 @@
+/// Provides a method on List to replace a single element in place.
 extension ListReplace<T> on List<T> {
+  /// Replaces [toReplace] in the List in place with [replacement] and returns
+  /// a resulting List.
   List<T> replaceSingle(T toReplace, T replacement) {
     assert(any((element) => element == toReplace));
     return map((e) => e == toReplace ? replacement : e).toList();
