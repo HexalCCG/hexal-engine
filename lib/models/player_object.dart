10e52f244187704f11babdee54c99eb4e2c663e4
Zoey Child <imperialoctopus@gmail.com>
2020-08-20 00:42:40 +0300

    Add generated files to gitignore

--- lib/models/player_object.dart
+++ lib/models/player_object.dart
@@ -1,7 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+
 import '../models/player.dart';
 import 'game_object.dart';
 
+/// Allows us to access JSON conversion methods in the generated file.
+part 'player_object.g.dart';
+
 /// GameObject to represent a player for targeting.
+@JsonSerializable()
 class PlayerObject extends GameObject {
   /// Which player this object represents.
   final Player player;
@@ -9,6 +15,15 @@ class PlayerObject extends GameObject {
   /// Represents one of the players.
   /// [id] should be 0 for Player One, and 1 for Player Two.
   const PlayerObject({required int id, required this.player}) : super(id: id);
+
+  /// Factory constructor for creating an instance of this from JSON.
+  factory PlayerObject.fromJson(Map<String, dynamic> json) =>
+      _$PlayerObjectFromJson(json);
+
+  /// `toJson` is the convention for a class to declare support for
+  /// serialization to JSON. The implementation simply calls the private,
+  /// generated helper method `_$GameStateToJson`.
+  Map<String, dynamic> toJson() => _$PlayerObjectFromJson(this);
 
   @override
   List<Object> get toStringProps => [id, player];
