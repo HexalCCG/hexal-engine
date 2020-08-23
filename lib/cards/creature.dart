9336c6083ea94c1019226c7e3ab0c7ff6afe3666
Zoey Child <imperialoctopus@gmail.com>
2020-08-24 00:22:38 +0300

    Replace creature and spell inheritance with mixins

--- lib/cards/creature.dart
+++ lib/cards/creature.dart
@@ -1,29 +1,18 @@
 import '../models/card_object.dart';
-import '../models/location.dart';
-import '../models/player.dart';
-import 'i_permanent.dart';
 
 /// Permanent card with stats that can attack and block attacks.
-abstract class Creature extends CardObject implements IPermanent {
+mixin Creature on CardObject {
+  @override
+  bool get permanent => true;
+
   /// Damage taken this turn.
-  final int damage;
+  int get damage;
 
   /// Determines what attacks are valid this turn.
-  final bool enteredFieldThisTurn;
+  bool get enteredFieldThisTurn;
 
   /// Whether this already attacked this turn.
-  final bool exhausted;
-
-  /// [id] must be unique. [owner] cannot be changed.
-  const Creature(
-      {required int id,
-      required Player owner,
-      required Player controller,
-      required Location location,
-      required this.damage,
-      required this.enteredFieldThisTurn,
-      required this.exhausted})
-      : super(id: id, owner: owner, controller: controller, location: location);
+  bool get exhausted;
 
   /// Health written on the card.
   int get baseHealth;
@@ -45,13 +34,4 @@ abstract class Creature extends CardObject implements IPermanent {
 
   /// Whether this can be targeted by attacks, and whether this blocks.
   bool get canBeAttacked => true;
-
-  /// Copy changing creature parameters [exhausted], [enteredFieldThisTurn],
-  /// [damage].
-  Creature copyWithCreature(
-      {bool? exhausted, bool? enteredFieldThisTurn, int? damage});
-
-  @override
-  List<Object> get toStringProps =>
-      [...super.toStringProps, damage, enteredFieldThisTurn, exhausted];
 }
