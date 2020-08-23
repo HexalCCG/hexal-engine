168aae33f951e32f0591ce2c89627a0de3744764
Zoey Child <imperialoctopus@gmail.com>
2020-08-24 00:22:38 +0300

    Replace creature and spell inheritance with mixins

--- lib/cards/i_permanent.dart
+++
@@ -1,2 +0,0 @@
-/// Remains on the field after play event resolves.
-abstract class IPermanent {}
--- lib/cards/spell.dart
+++ lib/cards/spell.dart
@@ -1,14 +1,7 @@
 import '../models/card_object.dart';
-import '../models/location.dart';
-import '../models/player.dart';
 
 /// Card with no stats that provides an effect.
-abstract class Spell extends CardObject {
-  /// [id] must be unique. [owner] cannot be changed.
-  const Spell(
-      {required int id,
-      required Player owner,
-      required Player controller,
-      required Location location})
-      : super(id: id, owner: owner, controller: controller, location: location);
+mixin Spell on CardObject {
+  @override
+  bool get permanent => false;
 }
