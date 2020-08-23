ffe3a373082c32f98965113c8f052ef4fc5dd3e4
Zoey Child <imperialoctopus@gmail.com>
2020-08-24 00:25:21 +0300

    Update cards to use mixins

--- lib/cards/sample/000_test_card.dart
+++ lib/cards/sample/000_test_card.dart
@@ -1,9 +1,10 @@
+import '../../models/card_object.dart';
 import '../../models/location.dart';
 import '../../models/player.dart';
 import '../spell.dart';
 
 /// Test spell with no effects or cost.
-class TestCard extends Spell {
+class TestCard extends CardObject with Spell {
   /// [id] must be unique. [owner] cannot be changed.
   const TestCard({
     required int id,
@@ -13,9 +14,13 @@ class TestCard extends Spell {
   }) : super(id: id, owner: owner, controller: controller, location: location);
 
   @override
-  TestCard copyWithBase({Player? controller, Location? location}) => TestCard(
-      id: id,
-      owner: owner,
-      controller: controller ?? this.controller,
-      location: location ?? this.location);
+  TestCard copyWith(Map<String, dynamic> changes) => TestCard(
+        id: changes['id'] as int? ?? id,
+        owner: changes['owner'] as Player? ?? owner,
+        controller: changes['controller'] as Player? ?? controller,
+        location: changes['location'] as Location? ?? location,
+      );
+
+  @override
+  List<Object> get props => [id, owner, controller, location];
 }
--- lib/cards/sample/001_cow_creature_card.dart
+++ lib/cards/sample/001_cow_creature_card.dart
@@ -1,26 +1,29 @@
+import '../../models/card_object.dart';
 import '../../models/location.dart';
 import '../../models/player.dart';
 import '../creature.dart';
 
 /// 2/2 Vanilla Creature.
-class CowCreatureCard extends Creature {
+class CowCreatureCard extends CardObject with Creature {
+  final bool exhausted;
+  final bool enteredFieldThisTurn;
+  final int damage;
+
   /// [id] must be unique. [owner] cannot be changed.
   const CowCreatureCard({
     required int id,
     required Player owner,
     required Player controller,
     required Location location,
-    required bool exhausted,
-    required bool enteredFieldThisTurn,
-    required int damage,
+    required this.exhausted,
+    required this.enteredFieldThisTurn,
+    required this.damage,
   }) : super(
-            id: id,
-            owner: owner,
-            controller: controller,
-            location: location,
-            exhausted: exhausted,
-            enteredFieldThisTurn: enteredFieldThisTurn,
-            damage: damage);
+          id: id,
+          owner: owner,
+          controller: controller,
+          location: location,
+        );
 
   @override
   int get baseAttack => 2;
@@ -28,25 +31,25 @@ class CowCreatureCard extends Creature {
   int get baseHealth => 2;
 
   @override
-  CowCreatureCard copyWithBase({Player? controller, Location? location}) =>
-      CowCreatureCard(
-          id: id,
-          owner: owner,
-          controller: controller ?? this.controller,
-          location: location ?? this.location,
-          exhausted: exhausted,
-          enteredFieldThisTurn: enteredFieldThisTurn,
-          damage: damage);
+  CowCreatureCard copyWith(Map<String, dynamic> changes) => CowCreatureCard(
+        id: changes['id'] as int? ?? id,
+        owner: changes['owner'] as Player? ?? owner,
+        controller: changes['controller'] as Player? ?? controller,
+        location: changes['location'] as Location? ?? location,
+        exhausted: changes['exhausted'] as bool? ?? exhausted,
+        enteredFieldThisTurn:
+            changes['enteredFieldThisTurn'] as bool? ?? enteredFieldThisTurn,
+        damage: changes['damage'] as int? ?? damage,
+      );
+
   @override
-  Creature copyWithCreature(
-          {int? damage, bool? enteredFieldThisTurn, bool? exhausted}) =>
-      CowCreatureCard(
-          id: id,
-          owner: owner,
-          controller: controller,
-          location: location,
-          exhausted: exhausted ?? this.exhausted,
-          enteredFieldThisTurn:
-              enteredFieldThisTurn ?? this.enteredFieldThisTurn,
-          damage: damage ?? this.damage);
+  List<Object> get props => [
+        id,
+        owner,
+        controller,
+        location,
+        exhausted,
+        enteredFieldThisTurn,
+        damage
+      ];
 }
