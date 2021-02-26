f95749e37532e19bb8257de70320c8fbb9a410e1
Zoey Child <imperialoctopus@gmail.com>
2021-02-26 14:00:49 +0000

    Add empty provide mana action

---
+++ lib/actions/provide_mana_action.dart
@@ -0,0 +1,35 @@
+import '../exceptions/action_exception.dart';
+import '../models/game_state.dart';
+import '../state_changes/state_change.dart';
+import 'action.dart';
+
+/// Channels a card to provide mana for the top stack event.
+class ProvideManaAction extends Action {
+  /// Card to channel.
+  final int card;
+
+  /// Channels a card to provide mana for the top stack event.
+  const ProvideManaAction({required this.card});
+
+  @override
+  bool valid(GameState state) {
+    return false;
+  }
+
+  @override
+  List<StateChange> apply(GameState state) {
+    if (!valid(state)) {
+      throw const ActionException(
+          'ProvideTargetAction Exception: invalid argument');
+    }
+
+    return [];
+  }
+
+  @override
+  List<Object> get props => [card];
+
+  /// Create from json.
+  static ProvideManaAction fromJson(List<dynamic> json) =>
+      ProvideManaAction(card: int.parse(json[0].toString()));
+}
