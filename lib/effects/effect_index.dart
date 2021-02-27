587604633f33f90fd93596f0358c3e513ca28be2
Zoey Child <imperialoctopus@gmail.com>
2021-02-27 12:50:53 +0000

    Add draw cards effect to index

--- lib/effects/effect_index.dart
+++ lib/effects/effect_index.dart
@@ -1,7 +1,9 @@
 import 'damage_effect.dart';
+import 'draw_cards_effect.dart';
 import 'effect.dart';
 
 /// Container for functions creating cards from json.
 Map<Type, Effect Function(List<dynamic>)> effectBuilders = {
   DamageEffect: DamageEffect.fromJson,
+  DrawCardsEffect: DrawCardsEffect.fromJson,
 };
