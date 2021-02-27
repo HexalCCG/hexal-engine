097983e32096c9af33a9ff60891d0303e640b73a
Zoey Child <imperialoctopus@gmail.com>
2021-02-27 13:04:47 +0000

    Index card builders by identity

--- lib/cards/01_basic/set_01.dart
+++ lib/cards/01_basic/set_01.dart
@@ -1,4 +1,14 @@
 import '../../models/card.dart';
+import '../../models/card_identity.dart';
+import '000_hivequeen_ngaat.dart';
+import '003_awakened_vines.dart';
+import '004_carnivorous_fern.dart';
+import '007_peach_sapling.dart';
 
-/// 00 Sample Cards
-const Map<int, Card Function(List<dynamic>)> set_01 = {};
+/// 01 Basic Set
+final Map<CardIdentity, Card Function(List<dynamic>)> set_01 = {
+  const CardIdentity(1, 0): HivequeenNgaat.fromJson,
+  const CardIdentity(1, 3): AwakenedVines.fromJson,
+  const CardIdentity(1, 4): CarnivorousFern.fromJson,
+  const CardIdentity(1, 7): PeachSapling.fromJson,
+};
