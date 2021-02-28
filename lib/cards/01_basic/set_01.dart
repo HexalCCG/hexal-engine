6c2592626242161d957296201f0a83452681e23e
Zoey Child <imperialoctopus@gmail.com>
2021-02-28 00:04:51 +0000

    Add new cards to set map

--- lib/cards/01_basic/set_01.dart
+++ lib/cards/01_basic/set_01.dart
@@ -4,6 +4,10 @@ import '000_hivequeen_ngaat.dart';
 import '003_awakened_vines.dart';
 import '004_carnivorous_fern.dart';
 import '007_peach_sapling.dart';
+import '009_large_worm.dart';
+import '015_ash_elemental.dart';
+import '016_armoured_crocodile.dart';
+import '019_protector_of_the_forest.dart';
 
 /// 01 Basic Set
 final Map<CardIdentity, Card Function(List<dynamic>)> set_01 = {
@@ -11,4 +15,8 @@ final Map<CardIdentity, Card Function(List<dynamic>)> set_01 = {
   const CardIdentity(1, 3): AwakenedVines.fromJson,
   const CardIdentity(1, 4): CarnivorousFern.fromJson,
   const CardIdentity(1, 7): PeachSapling.fromJson,
+  const CardIdentity(1, 9): LargeWorm.fromJson,
+  const CardIdentity(1, 15): AshElemental.fromJson,
+  const CardIdentity(1, 16): ArmouredCrocodile.fromJson,
+  const CardIdentity(1, 19): ProtectorOfTheForest.fromJson,
 };
