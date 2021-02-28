2aa172f8361a414d2b695be42d1411cd18f78e0a
Zoey Child <imperialoctopus@gmail.com>
2021-02-28 17:45:31 +0000

    Add new event state to resolve state change

--- lib/state_changes/resolve_event_state_change.dart
+++ lib/state_changes/resolve_event_state_change.dart
@@ -1,7 +1,8 @@
 import '../events/event.dart';
 import '../exceptions/state_change_exception.dart';
+import '../extensions/replace_single.dart';
+import '../models/enums/event_state.dart';
 import '../models/game_state.dart';
-import 'remove_event_state_change.dart';
 import 'state_change.dart';
 
 /// StateChange to resolve an event.
@@ -9,23 +10,29 @@ class ResolveEventStateChange extends StateChange {
   /// Event to resolve.
   final Event event;
 
+  /// Resolution state to set it to.
+  final EventState eventState;
+
   /// Sets [event] to resolved.
-  const ResolveEventStateChange({required this.event});
+  const ResolveEventStateChange(
+      {required this.event, required this.eventState});
 
   @override
   GameState apply(GameState state) {
     if (!state.stack.contains(event)) {
-      throw (const StateChangeException('Event not found in stack.'));
+      throw const StateChangeException('Event not found in stack.');
     }
 
-    return RemoveEventStateChange(event: event).apply(state);
+    if (eventState == EventState.unresolved) {
+      throw const StateChangeException('Cannot change event to unresolved.');
+    }
 
-    // Old resolve pattern
-    /*
-    final newEvent = state.getEvent(event).copyResolved;
+    // Removing event.
+    //return RemoveEventStateChange(event: event).apply(state);
+
+    final newEvent = state.getEvent(event).copyWith(state: eventState);
     final newStack = state.stack.replaceSingle(event, newEvent).toList();
     return state.copyWith(stack: newStack);
-    */
   }
 
   @override
