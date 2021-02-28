fb5130ac61bdef67daef7644d04d7cca94e127ff
Zoey Child <imperialoctopus@gmail.com>
2021-02-28 17:44:17 +0000

    Change draw cards effect to extend the event version

--- lib/effects/draw_cards_effect.dart
+++ lib/effects/draw_cards_effect.dart
@@ -1,52 +1,26 @@
 import '../events/draw_cards_event.dart';
-import '../events/event.dart';
 import '../models/enums/event_state.dart';
 import '../models/enums/player.dart';
 import '../models/game_state.dart';
-import '../state_changes/add_event_state_change.dart';
-import '../state_changes/resolve_event_state_change.dart';
 import '../state_changes/state_change.dart';
 import 'effect.dart';
 
 /// Deals damage to a target.
-class DrawCardsEffect extends Event with Effect {
-  /// Number of cards to draw.
-  final int draws;
-
-  /// Player who draws the cards.
-  final Player player;
-
+class DrawCardsEffect extends DrawCardsEvent with Effect {
   /// [target] is target to request. [targetResult] returns from the request.
   /// [targetIndex] counts through list of targets to apply damage.
   const DrawCardsEffect({
     int id = 0,
     EventState state = EventState.unresolved,
-    required this.draws,
-    required this.player,
-  }) : super(id: id, state: state);
+    required int draws,
+    required Player player,
+  }) : super(id: id, state: state, draws: draws, player: player);
 
   @override
-  bool valid(GameState state) {
-    return true;
-  }
+  bool valid(GameState state) => super.valid(state);
 
   @override
-  List<StateChange> apply(GameState state) {
-    if (!valid(state)) {
-      return [ResolveEventStateChange(event: this)];
-    }
-
-    if (draws <= 0) {
-      // If there are no targets, resolve this.
-      return [ResolveEventStateChange(event: this)];
-    }
-
-    // Replace this with a draw cards event.
-    return [
-      AddEventStateChange(event: DrawCardsEvent(player: player, draws: draws)),
-      ResolveEventStateChange(event: this)
-    ];
-  }
+  List<StateChange> apply(GameState state) => super.apply(state);
 
   @override
   DrawCardsEffect copyWith({int? id, EventState? state}) => DrawCardsEffect(
