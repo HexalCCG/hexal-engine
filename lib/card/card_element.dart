5d009e922f3af18fe5907c4abfcf0a89d4d695f1
Zoey Child <imperialoctopus@gmail.com>
2021-03-03 21:42:15 +0000

    Add card export

--- lib/card/card_element.dart
+++
@@ -1,9 +0,0 @@
-import 'card.dart';
-import '../model/enums/element.dart';
-import '../model/mana_amount.dart';
-
-/// Provides defaults for a card with an element.
-mixin CardElement<T extends Element> on Card {
-  @override
-  ManaAmount get providesMana => throw UnimplementedError();
-}
--- lib/hexal_engine.dart
+++ lib/hexal_engine.dart
@@ -1,4 +1,5 @@
 export 'action/action_index.dart';
+export 'card_data/card_index.dart';
 export 'effect/effect_index.dart';
 export 'event/event_index.dart';
 export 'exceptions/exceptions.dart';
