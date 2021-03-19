ddc068d05959c82607368b1a2db1c572a3b3a483
Zoey Child <imperialoctopus@gmail.com>
2021-03-19 23:01:03 +0000

    Add validate methods to deck code class

--- lib/model/deck.dart
+++ lib/model/deck.dart
@@ -1,4 +1,6 @@
 import '../card/card.dart';
+import '../exceptions/exceptions.dart';
+import '../extensions/max_duplicates.dart';
 import 'card_identity.dart';
 import 'enums/location.dart';
 import 'enums/player.dart';
@@ -23,7 +25,12 @@ class Deck {
   static Deck fromCode(String code) {
     final identities = code.split(',').map((cardCode) {
       final list = cardCode.split('.');
-      return CardIdentity(int.parse(list[0]), int.parse(list[1]));
+
+      try {
+        return CardIdentity(int.parse(list[0]), int.parse(list[1]));
+      } on FormatException {
+        throw DeckCodeException('Error parsing card: "' + cardCode + '"');
+      }
     }).toList();
     return Deck(identities: identities);
   }
@@ -40,5 +47,23 @@ class Deck {
       return card.copyWith(
           owner: player, controller: player, location: Location.deck);
     }).toList();
+  }
+
+  /// Validates a deck code.
+  static void validateCode(String code) {
+    final deck = fromCode(code);
+    deck.validate();
+  }
+
+  /// Validates a deck.
+  void validate() {
+    final cards = toCards(2, Player.one);
+    if (cards.length != 30) {
+      throw const DeckCodeException('Deck must contain exactly 30 cards.');
+    }
+    if (identities.maxDuplicates() > 2) {
+      throw const DeckCodeException(
+          'Deck may not contain more than 2 of any one card.');
+    }
   }
 }
